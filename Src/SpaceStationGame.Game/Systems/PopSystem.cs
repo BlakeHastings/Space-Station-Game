6be0e01d2a1700d8267895e8c887ee076b9212d9
Blake Hastings <blakehastings@outlook.com>
2025-12-09 15:48:56 -0600

    - fixed popSystem

--- Src/SpaceStationGame.Game/Systems/PopSystem.cs
+++ Src/SpaceStationGame.Game/Systems/PopSystem.cs
@@ -1,12 +1,25 @@
 using SpaceStationGame.Engine;
+using SpaceStationGame.Engine.Events;
 using SpaceStationGame.Engine.Systems;
 
 namespace SpaceStationGame.Game;
 
 public class PopSystem : ISystem
 {
+
+    public readonly EventBus EventBus;
+    public PopSystem(EventBus eventBus)
+    {
+        EventBus = eventBus;
+    }
+
     public void Update(double timeStepMs, CancellationToken cancellationToken)
     {
-        Console.WriteLine($"{nameof(PopSystem)} Update Call {timeStepMs}");
+        EventBus.Emit(new PopUpdateEvent(timeStepMs));
     }
+}
+
+public record PopUpdateEvent(double timeStepMs) : IEventData
+{
+    public static int EventTypeId => 1;
 }
