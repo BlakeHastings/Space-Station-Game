07521d159db975790f7bb1ed0cdf13548eed8e1f
Marco Dietrich <marcomad15@web.de>
2025-12-11 14:47:41 +0100

    the stuff we did tohger added reoucres compoent    just name +  weith in kg then added them to the inventory
    then added the conseol print ot print the inveintry
    
    and yes this is not how you do git commity but   whateer

---
+++ Src/SpaceStationGame.Game/Components/RecourseComponent.cs
@@ -0,0 +1,13 @@
+using Arch.Core;
+
+namespace SpaceStationGame.Game.Components;
+
+public struct RecourseComponent
+{
+    // Resource identifier, e.g. "iron_ore"
+    public string ResourceName;
+
+    // Weight in kilograms
+    public double WeightKg;
+
+}
